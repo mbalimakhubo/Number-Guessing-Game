061ed3a5e815642c95d89be8e44e186011a30fef
MbaliMakhubo98 <mbali.makhubo25@gmail.com>
2025-01-23 22:41:36 +0200

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,3 +1,4 @@
 #This is a Number Guessing Game
 
-print('Welcome to the Number Guessing Game')
+print("Welcome to the Number Guessing Game")
+print("Let's see if you can choose a number from 1 to 100.")
